5b25495a1a909ab695e659b8bda50a01b1117b9b
GerardoHau <gerardohau97@gmail.com>
2020-04-17 10:43:12 -0500

    test de database manager

--- tests/DatabaseManagerTest.php
+++ tests/DatabaseManagerTest.php
@@ -2,12 +2,37 @@
 use PHPUnit\Framework\TestCase;
 final class DatabaseManagerTest extends TestCase
 {
-    public function testActualizar(){
-      $actualiza=new Suma();
-     
+    public function testInsertQuery(){
+      $database = DataBaseManager::getInstance();
       $this->assertEquals(  
-         true,$actualiza->actualizar(1,2)
+         true, $database->insertQuery("INSERT INTO materias(nombre) VALUES ('Psicologia')")
       );
     }
+   
+    public function testInsertQueryFailed(){
+      $database = DataBaseManager::getInstance();
+      $this->assertEquals(  
+         false, $database->insertQuery("INSERT INTO salones(nombre) VALUES ('Psicologia')")
+      );
+    }
+
+    public function testRealizeQuery(){
+      $database = DataBaseManager::getInstance();
+      $result = $database->realizeQuery("SELECT * FROM materias where id = 2");
+      $this->assertEquals(  
+         "Semat", $result[0]['nombre']
+      );
+    }
+
+    public function testRealizeQueryFailed(){
+      $database = DataBaseManager::getInstance();
+      $result = $database->realizeQuery("SELECT * FROM materias where id = 40");
+      $this->assertEquals(  
+         true, empty($result)
+      );
+    }
+    
+
+    
 }
 ?>
